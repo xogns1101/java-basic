c3fc976b13264baca5ffb49ce37e37bcc504c8bc
taehun <cocoseasky@naver.com>
2024-02-20 14:33:42 +0900

    수정중

--- src/datatype/TextualExample.java
+++ src/datatype/TextualExample.java
@@ -5,7 +5,10 @@ public class TextualExample {
     public static void main(String[] args) {
 
         char c1 = 'A';
+        System.out.println("c1 = " + c1);
 
+        char c2 = 65;
+        System.out.println("c2 = " + c2);
 
 
     }
