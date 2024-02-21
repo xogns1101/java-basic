a554e611fdb5eee86601e2112d5961ef3fa33720
taehun <cocoseasky@naver.com>
2024-02-21 10:37:13 +0900

    연산자 수업종료

---
+++ src/basic/operator/UnaryOperator.java
@@ -0,0 +1,28 @@
+package basic.operator;
+
+public class UnaryOperator {
+
+    public static void main(String[] args) {
+
+        // 증감 연산자 (++, --)
+        // 변수의 값을 단순히 하나 올리거나 내릴 떄 사용합니다.
+
+        int i = 1;
+        int j = i++; // 후위 연산 (선 연산 후 증감)
+        int k = i--;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+
+        System.out.println("--------------------------------");
+
+        int x = 1;
+        int y = ++x;
+        int z = --x;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+
+}
